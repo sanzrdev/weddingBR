d3c74cb7546fbb09140f18ef733eb255684f0c7a
Roldan Sanz <roldansantsz@gmail.com>
2026-09-04 12:26:36 -0600

    pagina con animaciones v1

---
+++ src/data/wedding.ts
@@ -0,0 +1,62 @@
+export const wedding = {
+  couple: {
+    bride: "Belen",
+    groom: "Roldan",
+  },
+
+  date: {
+    day: "26",
+    month: "DICIEMBRE",
+    year: "2026",
+    countdownTarget: "2026-12-26T15:30:00-06:00",
+  },
+
+  rsvp: {
+    phone: "529711584878",
+  },
+
+  music: {
+    spotifyEmbedUrl: "https://open.spotify.com/embed/track/1EZypwk0xcj64ZLAglhLs2?utm_source=generator",
+    spotifyUrl: "https://open.spotify.com/intl-es/track/1EZypwk0xcj64ZLAglhLs2?si=78bf3b93936a46e5",
+  },
+
+ events: [
+    {
+      type: "Boda religiosa",
+      time: "13:45",
+      place: "Parroquia de San Jerónimo Dr.",
+      mapUrl: "https://maps.app.goo.gl/cYY3ZHtMKMSpPBqT7",
+    },
+    {
+      type: "Recepción",
+      time: "15:30",
+      place: "Salón Kareka",
+      mapUrl: "https://maps.app.goo.gl/12tXRu7s4eELXyBt8",
+    },
+    {
+      type: "Ceremonia civil",
+      time: "16:00",
+      place: "Salón Kareka",
+      mapUrl: "https://maps.app.goo.gl/12tXRu7s4eELXyBt8",
+    },
+  ],
+
+   story: {
+    image: "/images/couple-01.jpg",
+  },
+
+  gallery: [
+    {
+        src: "/images/couple-01.jpg",
+        alt: "B&R"
+    },
+    {
+        src: "/images/couple-02.jpg",
+        alt: "B&R"
+    },
+    {
+        src: "/images/couple-03.jpg",
+        alt: "B&R"
+    },
+  ]
+};
